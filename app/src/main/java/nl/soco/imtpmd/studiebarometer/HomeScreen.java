3addc7862144ffdbfbc53b929bb82cc89c230b73
Soed <soewd>
2016-03-04 14:46:30 +0100

    First test

--- app/src/main/java/nl/soco/imtpmd/studiebarometer/HomeScreen.java
+++ app/src/main/java/nl/soco/imtpmd/studiebarometer/HomeScreen.java
@@ -3,7 +3,7 @@ package nl.soco.imtpmd.studiebarometer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
