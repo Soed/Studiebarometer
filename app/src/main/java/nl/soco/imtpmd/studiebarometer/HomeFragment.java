bedfc96796438376d264be275f4fae28dec3da94
collinnl <collinwoerde@gmail.com>
2016-05-13 16:00:22 +0200

    menu nu met home style en menu zelf opgemaakt, volgende keer verder met naam ophalen en email, eerst data locaal opslaan in sharedprefs...

--- app/src/main/java/nl/soco/imtpmd/studiebarometer/HomeFragment.java
+++ app/src/main/java/nl/soco/imtpmd/studiebarometer/HomeFragment.java
@@ -3,13 +3,11 @@ package nl.soco.imtpmd.studiebarometer;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by Collin on 19-4-2016.
- */
 public class HomeFragment extends Fragment {
 
     View myView;
