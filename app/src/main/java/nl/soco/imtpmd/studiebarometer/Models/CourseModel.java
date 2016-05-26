b975e3e55352cf76df80fbd227a94bc5aa07d4e2
Soed <soedhanand@gmail.com>
2016-05-26 02:05:06 +0200

    Get JSON werkt, Parse JSON werkt bijna...

--- app/src/main/java/nl/soco/imtpmd/studiebarometer/Models/CourseModel.java
+++ app/src/main/java/nl/soco/imtpmd/studiebarometer/Models/CourseModel.java
@@ -3,22 +3,49 @@ import java.io.Serializable;
 
 public class CourseModel implements Serializable {           // WAAROM serializable ????
 
-    public String name;
-    public String ects;
-    public String grade;
-    public String period;
-    public String code;
+    private String name;
+    private int ects;
+    private int grade;
+    private int period;
 
 
-    public CourseModel(String courseName, String ects, String grade, String period){
+    public CourseModel(String courseName, int ects, int grade, int period){
         this.name = courseName;
         this.ects = ects;
         this.grade = grade;
         this.period = period;
-        this.code = code;
     }
 
-    // ADD GETTERS
+    public String getName() {
+        return name;
+    }
 
-    // ADD SETTERS
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEcts() {
+        return ects;
+    }
+
+    public void setEcts(int ects) {
+        this.ects = ects;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(int period) {
+        this.period = period;
+    }
+
 }
